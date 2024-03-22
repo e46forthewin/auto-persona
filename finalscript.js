442a8ed41df67703cefc5039048e490390f44336
skrishn <skrishn@lucidmotors.com>
2024-03-21 17:50:38 -0700

    auto_theme

--- finalscript.js
+++ finalscript.js
@@ -2,25 +2,25 @@
 
 const questions = [
     {
-        question: "Q1/12: You're taking the bus on your way back home. It's been a loooong day, what are you thinking about?",
+        question: "Q1/12: You're taking a Dodge Caravan carpoool on your way back home. It's been a loooong day, what are you thinking about?",
         image: "images/Q1.gif",
         answers: {
             F: {
-                text: "I'm just so tired and can't wait to go home. Why do everyday feel the same...",
+                text: "I'm just so tired and can't wait to drive my Prelude. Why does everyday feel the same...",
                 scores: { T: 0, F: +1 },
             },
             T: {
-                text: "Should I rest or work first when I get home? Most importantly, what's for dinner?",
+                text: "Should I rest or work first when I get home? Most importantly, is the R34 better than the Mk4 Supra?",
                 scores: { T: +1, F: 0 },
             },
         },
     },
     {
-        question: "Q2/12: You look outside the windows, seems like it's going to rain soon. Suddenly everything turns into darkness!!",
+        question: "Q2/12: You look outside the windows, seems like you're abt to drag race a Civic with a laptop. Suddenly everything turns into darkness!!",
         image: "images/Q2.gif",
         answers: {
             J: {
-                text: "I don't remember this tunnel. *Pull out your phone to look at the map*",
+                text: "VAMOSSSS!!! *Pull out your phone to record a video*",
                 scores: { P: 0, J: +1 },
             },
             P: {
@@ -30,7 +30,7 @@ const questions = [
         },
     },
     {
-        question: "Q3/12: Before you can do anything, the bus stops and everything around you floats up!",
+        question: "Q3/12: Before you can do anything, the Caravan stops and everything around you floats up!",
         image: "images/Q3.gif",
         answers: {
             S: {
@@ -38,7 +38,7 @@ const questions = [
                 scores: { S: +1, N: 0 },
             },
             N: {
-                text: "Is this a prank? Am I getting kidnapped by aliens?!",
+                text: "Is this a prank? Am I winning?!",
                 scores: { S: 0, N: +1 },
             },
         },
@@ -48,21 +48,21 @@ const questions = [
         image: "images/Q4.gif",
         answers: {
             F: {
-                text: "Woahhh...so many cats! How cute!! I wanna give them a hug!",
+                text: "Woahhh...so many Miatas! How cute!! I wanna give them a hug!",
                 scores: { T: 0, F: +1 },
             },
             T: {
-                text: "Cats? In space? That's kinda odd... How do they survive out there?!",
+                text: "Miatas? In space? That's kinda odd... How do they survive out there?!",
                 scores: { T: +1, F: 0 },
             },
         },
     },
     {
-        question: "Q5/12: 'Welcome Aboard!' a small robot appears and annouces, 'We will reach our destination in 5 months 6 days and 2 hours!'",
+        question: "Q5/12: 'Welcome Aboard!' a small Isetta appears and annouces, 'We will reach our destination in 5 months 6 days and 2 hours!'",
         image: "images/Q5.gif",
         answers: {
             P: {
-                text: "Cool! A space cruise?! Where are we going? Do I get a spacesuit? This is so exciting!",
+                text: "Cool! A space cruise?! Where are we going? Do I get a racesuit? This is so exciting!",
                 scores: { P: +1, J: 0 },
             },
             J: {
@@ -72,21 +72,21 @@ const questions = [
         },
     },
     {
-        question: "Q6/12: The robot just points to the back of the bus which now looks more like inside of a spaceship! What do you want to see first?",
+        question: "Q6/12: The robot just points to the back of the Dodge which now looks more like inside of a spaceship! What do you want to see first?",
         image: "images/Q6.gif",
         answers: {
             I: {
-                text: "Solar system themed fountain in the middle. I want to take pictures with it.",
+                text: "997 GT3 RS. I want to take pictures with it.",
                 scores: { I: 0, E: +1 },
             },
             E: {
-                text: "Cozy corner with a massaging chair. I need to sit down and process.",
+                text: "Una Lucid Sapphire para mi por favor.",
                 scores: { I: +1, E: 0 },
             },
         },
     },
     {
-        question: "Q7/12: On the way back to your seat, you are surrounded by cats! 'Unauthorized passenger detected, CAPTURE CAPTURE!'",
+        question: "Q7/12: On the way back to your seat, you are surrounded by Tesla Cybertrucks! 'Unauthorized passenger detected, CAPTURE CAPTURE!'",
         image: "images/Q7.gif",
         answers: {
             S: {
@@ -100,7 +100,7 @@ const questions = [
         },
     },
     {
-        question: "Q8/12: Suddenly, the robot glides in, it whispers something to the cats that makes them stop.",
+        question: "Q8/12: Suddenly, the S2000 glides in, it whispers something to the E39 M5 that makes it stop.",
         image: "images/Q8.gif",
         answers: {
             E: {
@@ -108,21 +108,21 @@ const questions = [
                 scores: { E: +1, I: 0 },
             },
             I: {
-                text: "HA! That's right! Back off, cats! Am I safe to go now?!",
+                text: "HA! That's right! Back off, E39! Am I safe to go now?!",
                 scores: { E: 0, I: +1 },
             },
         },
     },
     {
-        question: "Q9/12: The robot transforms into a giant vacuum cleaner, the cats use it to point at you!",
+        question: "Q9/12: The Cybertruck is already a giant vacuum cleaner, the E39 uses it to point at you!",
         image: "images/Q9.gif",
         answers: {
             J: {
-                text: "Throw your coat at them as distraction, run towards the big green 'EXIT' sign!",
+                text: "Throw your coat at them as distraction, run towards the big blue 'VOLVO' sign!",
                 scores: { J: +1, P: 0 },
             },
             P: {
-                text: "Run in random direction, confuse them, blend in with the crowd!",
+                text: "Run in random direction, confuse them, blend in with the white Tesla Model 3s!",
                 scores: { J: 0, P: +1 },
             },
         },
@@ -132,18 +132,18 @@ const questions = [
         image: "images/Q10.gif",
         answers: {
             F: {
-                text: "Get the Jupiter headphone, wear it to hear people's thoughts!",
+                text: "Get the Camry, let's head home in one piece",
                 scores: { F: +1, T: 0 },
             },
             T: {
-                text: "Get the Mars watch, turn it and you can time travel!",
+                text: "Get the E60 M5",
                 scores: { F: 0, T: +1 },
             },
         },
 
     },
     {
-        question: "Q11/12: The giant vacuum cleaner suddenly emerges in front of you! You're sucked into its vortex!!!",
+        question: "Q11/12: The Dodge Viper SRT-10 suddenly emerges in front of you! You're sucked into its intake manifold!!!",
         image: "images/Q11.gif",
         answers: {
             S: {
@@ -151,31 +151,31 @@ const questions = [
                 scores: { S: +1, N: 0 },
             },
             N: {
-                text: "I'll find the way out! Maybe there are some secret doors! *look around for the way out*",
+                text: "There's enough room here, it's a frickin 8.4L monster. I be chillin",
                 scores: { S: 0, N: +1 },
             },
         },
     },
     {
-        question: "Q12/12: After a moment of darkness, you find yourself back in the bus. 'Are you alright?' the ticket inspector asks",
+        question: "Q12/12: After a moment of darkness, you find yourself back in the Dodge Caravan. 'Are you alright?' the HR asks",
         image: "images/Q12.gif",
         answers: {
             I: {
-                text: "Woah! I thought I was captured... Nevermind! I'm alright now, I guess? *panic in silence*",
+                text: "Woah! I thought BMW actually made the G80/G82 pigs... Nevermind! I'm alright now, I guess? *panic in silence*",
                 scores: { E: 0, I: +1 },
             },
             E: {
-                text: "You won't believe it...I think I just had the wildest dream ever! *tell him about your space adventure*",
+                text: "You won't believe it...I think I just had the wildest dream ever! *tell him about Lucid stock hitting USD 1000*",
                 scores: { E: +1, I: 0 },
             },
         },
     },
     {
-        question: "When you look up again, the inspector's already gone. You glance outside the window, darkness creeps in as the bus enters another tunnel...",
+        question: "When you look up again, the Gravity is the highest selling vehicle in the US of A. You glance outside the window, darkness creeps in as the bus enters another tunnel...",
         image: "images/Q13.gif",
         answers: {
             K: {
-                text: "Wait...was that Bob?",
+                text: "Wait...was that Isetta?",
             },
             U: {
                 text: "Oh no...Do I have to go through this again?",
@@ -186,52 +186,52 @@ const questions = [
 
 const resultOptions = {
     "ISTJ": {
-        image: "1DE.png",
+        image: "1DE.jpg",
     },
     "ISFJ": {
-        image: "2light.png"
+        image: "2light.jpg"
     },
     "INFJ": {
-        image: "3UFO.png"
+        image: "3UFO.jpg"
     },
     "INTJ": {
-        image: "4nebula.png"
+        image: "4nebula.jpg"
     },
     "ISTP": {
-        image: "5comet.png"
+        image: "5comet.jpg"
     },
     "ISFP": {
-        image: "6ST.png"
+        image: "6ST.jpg"
     },
     "INFP": {
-        image: "7DM.png"
+        image: "7DM.jpg"
     },
     "INTP": {
-        image: "8met.png"
+        image: "8met.jpg"
     },
     "ESTP": {
-        image: "9BH.png"
+        image: "9BH.jpg"
     },
     "ESFP": {
-        image: "10Sn.png"
+        image: "10Sn.jpg"
     },
     "ENFP": {
-        image: "11Grav.png"
+        image: "11Grav.jpg"
     },
     "ENTP": {
-        image: "12hand.png"
+        image: "12hand.jpg"
     },
     "ESTJ": {
-        image: "13sat.png"
+        image: "13sat.jpg"
     },
     "ESFJ": {
-        image: "14sun.png"
+        image: "14sun.jpg"
     },
     "ENFJ": {
-        image: "15gal.png"
+        image: "15gal.jpg"
     },
     "ENTJ": {
-        image: "16rocket.png"
+        image: "16rocket.jpg"
     },
 
 };
